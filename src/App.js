cfbb3f0ca9fddbcda82985f9e650b1699378c2aa
Felipe Miranda de Lima <fmlima4@outlook.com>
2021-08-10 12:16:44 -0300

    darkTheme e ajustes gerais de layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-import React, {useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css'
 import Crypto from './Crypto';
-import Header from './components/Header';
 import Cryptos from './components/Cryptos';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from './theme';
+import { withStyles } from '@material-ui/core/styles';
 
 export default () => {
+  const [theme, setTheme] = useState("dark");
+  const isDarkTheme = theme === "dark";
+  const toggleTheme = () => setTheme(isDarkTheme ? "light" : "dark");
+  const IOSSwitch = withStyles((theme) => ({
+    root: {
+      width: 42,
+      height: 26,
+      padding: 0,
+      margin: theme.spacing(1),
+    },
+    switchBase: {
+      padding: 1,
+      '&$checked': {
+        transform: 'translateX(16px)',
+        color: theme.palette.common.white,
+        '& + $track': {
+          backgroundColor: '#52d869',
+          opacity: 1,
+          border: 'none',
+        },
+      },
+      '&$focusVisible $thumb': {
+        color: '#52d869',
+        border: '6px solid #fff',
+      },
+    },
+    thumb: {
+      width: 24,
+      height: 24,
+    },
+    track: {
+      borderRadius: 26 / 2,
+      border: `1px solid ${theme.palette.grey[400]}`,
+      backgroundColor: theme.palette.grey[50],
+      opacity: 1,
+      transition: theme.transitions.create(['background-color', 'border']),
+    },
+    checked: {},
+    focusVisible: {},
+  }))(({ classes, ...props }) => {
+    return (
+      <Switch
+        focusVisibleClassName={classes.focusVisible}
+        disableRipple
+        classes={{
+          root: classes.root,
+          switchBase: classes.switchBase,
+          thumb: classes.thumb,
+          track: classes.track,
+          checked: classes.checked,
+        }}
+        {...props}
+      />
+    );
+  });
 
   const[criptoList,setCriptoList] = useState([]);
   const[darkMode,setDarkMode] = useState(true);
@@ -19,18 +79,30 @@ export default () => {
     loadAll();
   } ,[])
 
-  return(
-    <div className="page">
-      <Header darkMode={darkMode}/>
+  return (
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <>
+        <GlobalStyles />
+        <header className={isDarkTheme ? 'darkTheme': 'lightTheme'}>
+          <h3>How much crypto can i buy ?</h3>
+          <FormControlLabel
+            control={<IOSSwitch checked={isDarkTheme} onChange={toggleTheme} name="isDarkmode" />}
+            label={isDarkTheme ? ' darkTheme': 'lightTheme'}
+          />
+        </header>
+        <div className="app--page">
 
-      <section className="lists">
-        {criptoList.map((item,key)=>(
-          <ul>
-            <Cryptos key={key} item={item}/>
-          </ul>
-        ))}
-      </section>
+          <section className="lists">
+            {criptoList.map((item,key)=>(
+              <ul>
+                <Cryptos key={key} item={item}/>
+              </ul>
+            ))}
+          </section>
 
-    </div>
-  )
+        </div>
+      </>
+    </ThemeProvider>
+  );
 }
+
