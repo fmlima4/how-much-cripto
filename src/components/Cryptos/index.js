f45c735b2eba7700f2984dc349829a13388be409
Felipe Miranda de Lima <fmlima4@outlook.com>
2021-08-14 11:38:26 -0300

    ajueste de calculo conversor

--- src/components/Cryptos/index.js
+++ src/components/Cryptos/index.js
@@ -7,31 +7,27 @@ export default ({item}) => {
     const[price,setPrice] = useState('');
     const[circulating_supply,setCirculatingSupply] = useState('');
     const[market_cap,setMarketCap] = useState('');
-    const[amountToBuy, setAmountToBuy ] = useState('')
-    const { amount, setAmount } = useContext(MyContext)
+    const { amount } = useContext(MyContext)
 
     useEffect(() => {
         const loadAll = async () =>{
-          
             let return_price = await Currency.format(item.price);
             let return_circulating_supply = await Currency.format(item.circulating_supply);
             let return_market_cap = await Currency.format(item.market_cap);
-            let return_amount = return_price.replace('$', '')
             setPrice(return_price); 
             setCirculatingSupply(return_circulating_supply); 
             setMarketCap(return_market_cap); 
-            setAmountToBuy(return_amount); 
             
         }
     
         loadAll();
-      } ,[])
+      } ,[item.market_cap,item.price,item.circulating_supply])
 
     return (
         <div className="card">
             <div className="card--header">
                 <img src={item.logo_url} alt="" />
-                <p>Voce pode comprar: {parseFloat(amount / amountToBuy).toFixed(2)}</p>
+                <p>Voce pode comprar: {parseFloat(amount / item.price).toFixed(2)}</p>
             </div>
             <div className="info">
                 <h2>{item.name}</h2>
